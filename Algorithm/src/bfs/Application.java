47779e96df9caa30b0998420d5e2eefed9ba5631
Ajay Pradeep <Ajay Pradeep@Ajay-PC>
2020-04-01 00:59:48 +0530

    BFS Algorithm Completed

--- Algorithm/src/bfs/Application.java
+++ Algorithm/src/bfs/Application.java
@@ -4,7 +4,15 @@ public class Application {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		BFS bfs = new BFS();
+		Vertex index1 = new Vertex(1);
+		Vertex index2 = new Vertex(2);
+		Vertex index3 = new Vertex(3);
+		Vertex index4 = new Vertex(4);
+		index1.addAdjacentIndex(index2);
+		index1.addAdjacentIndex(index3);
+		index2.addAdjacentIndex(index4);
+		bfs.bfs(index1);
 	}
 
 }
--- Algorithm/src/bfs/BFS.java
+++ Algorithm/src/bfs/BFS.java
@@ -1,10 +1,27 @@
 package bfs;
 
+import java.util.LinkedList;
+import java.util.Queue;
+
 public class BFS {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	public void bfs(Vertex vertexIndex) {
+		Queue<Vertex> queuebfs = new LinkedList<>();
+		queuebfs.add(vertexIndex);
+		vertexIndex.setVisitedNode(true);
+		System.out.println(vertexIndex.getVertexData()+" Root");
+		while(!queuebfs.isEmpty()) {
+			Vertex rootVertex = queuebfs.remove();
+			for(Vertex neighbor : rootVertex.getAdjacentIndex()) {
+				if(!neighbor.isVisitedNode()) {
+					neighbor.setVisitedNode(true);
+					queuebfs.add(neighbor);
+					System.out.print(neighbor.getVertexData()+" ");
+				}
+				
+			}
+			System.out.println("");
+		}
 	}
 
 }
