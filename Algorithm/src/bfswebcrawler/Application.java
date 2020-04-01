3dce36eb4bbdd7bf4078fd888a0c7a12b13aa46f
Ajay Pradeep <Ajay Pradeep@Ajay-PC>
2020-04-01 23:10:23 +0530

    Initial commit for Webcrawler bfs algorithm

---
+++ Algorithm/src/bfswebcrawler/Application.java
@@ -0,0 +1,14 @@
+package bfswebcrawler;
+
+import java.util.Scanner;
+
+public class Application {
+	public static void main(String[] args) {
+		WebCrawler crawl = new WebCrawler();
+		Scanner s = new Scanner(System.in);
+		System.out.println("Enter a url (ex:http://www.exmaple.com)");
+		String myurl = s.nextLine();
+		crawl.doWebCrawling(myurl);
+		
+	}
+}
---
+++ Algorithm/src/bfswebcrawler/WebCrawler.java
@@ -0,0 +1,57 @@
+package bfswebcrawler;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class WebCrawler {
+
+	private Queue<String> initialAndAddedQueue;
+	private List<String> discoveredWebSites;
+	
+	public WebCrawler() {
+		this.discoveredWebSites = new ArrayList<String>();
+		this.initialAndAddedQueue = new LinkedList<String>();
+	}
+	
+	public void doWebCrawling(String userInputSingleWebsite) {
+		initialAndAddedQueue.add(userInputSingleWebsite);
+		while(!initialAndAddedQueue.isEmpty()) {
+			
+			String urlFromQueue = initialAndAddedQueue.remove();
+			String wholeHtmlOfURL = readURL(urlFromQueue);
+			String regrex = "http://(\\w+\\.)*(\\w+)";
+			Pattern givenPattern = Pattern.compile(regrex);
+			Matcher matcher = givenPattern.matcher(wholeHtmlOfURL);
+			
+			while(matcher.find()) {
+				String foundURL = matcher.group();
+				this.discoveredWebSites.add(foundURL);
+				System.out.println("Website found : "+foundURL);
+				initialAndAddedQueue.add(foundURL); 
+			}
+		}
+	}
+
+	private String readURL(String urlFromQueue) {
+		String rawHtml = "";
+		try {
+			URL url = new URL(urlFromQueue);
+			BufferedReader in = new BufferedReader(new InputStreamReader(url.openStream()));
+			String toReadLineByLine = "";
+			while((toReadLineByLine = in.readLine()) != null) {
+				rawHtml += toReadLineByLine;
+			}
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return rawHtml;
+	}
+}
